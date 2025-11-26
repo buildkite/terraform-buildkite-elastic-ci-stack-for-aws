3838553b1500c22881483627cef2e0ae87954c09
Łukasz Jendrysik <lukasz@buildkite.com>
2025-11-26 12:22:08 +0100

    Update module tag in examples

--- examples/spot-instances/main.tf
+++ examples/spot-instances/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 module "buildkite_stack" {
-  source = "github.com/buildkite/terraform-buildkite-elastic-ci-stack-for-aws?ref=v0.3.1"
+  source = "github.com/buildkite/terraform-buildkite-elastic-ci-stack-for-aws?ref=v0.4.0"
 
   stack_name            = "buildkite-spot"
   buildkite_queue       = "spot"
