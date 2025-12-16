59b7003f2ede8a9fb5189ec4141c9db4e9187095
Joe Coleman <joe@buildkite.com>
2025-12-16 15:55:05 +0000

    Temp override to allow update of AMIs

--- .buildkite/scripts/check_version_change.sh
+++ .buildkite/scripts/check_version_change.sh
@@ -23,4 +23,21 @@ steps:
 EOF
 else
   echo "cloudformation_stack_version unchanged, skipping AMI update" >&2
+  cat <<EOF | buildkite-agent pipeline upload
+steps:
+  - label: "Update AMI Mappings"
+    plugins:
+      - docker#v5.13.0:
+          image: hashicorp/terraform:1.13
+          workdir: "/workdir"
+          entrypoint: "/bin/sh"
+          command: ["-c", "apk add --no-cache bash curl git openssh && bash .buildkite/scripts/check_ami_version_match.sh"]
+          environment:
+            - DEPLOY_KEY
+            - BUILDKITE_BRANCH
+    secrets:
+        DEPLOY_KEY: ES_FOR_TF_PUB_KEY
+    agents:
+      queue: "oss-deploy"
+EOF
 fi
