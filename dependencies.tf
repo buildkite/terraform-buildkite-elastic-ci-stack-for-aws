e16b83013a5589c0762b6dfa7f6360e310697c02
Łukasz Jendrysik <lukasz@buildkite.com>
2026-07-23 11:28:22 +0200

    Add example with custom_user_data and custom image

--- dependencies.tf
+++ dependencies.tf
@@ -3,4 +3,11 @@ data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 data "aws_availability_zones" "available" {
   state = "available"
+
+  # Only use standard regional Availability Zones. Opted-in edge zones may
+  # not support regional services such as the SSM VPC endpoints.
+  filter {
+    name   = "zone-type"
+    values = ["availability-zone"]
+  }
 }
